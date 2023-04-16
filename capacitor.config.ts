67ca4a7de2db911a60d03762d4e26601b3ebc843
Ramana <ramans25n@gmail.com>
2023-04-16 11:16:09 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TailorManagementApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
